536db141e9939372ca10f30052440fdb1440c1f4
lkp25 <lkp25@o2.pl>
2021-08-30 15:43:14 +0200

    contitional display recipie list components with ngFor and property binding added

--- src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
+++ src/app/recipies/recipie-list/recipie-item/recipie-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Recipie } from '../../recipe-model';
 
 @Component({
   selector: 'app-recipie-item',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipie-item.component.css']
 })
 export class RecipieItemComponent implements OnInit {
-
+  @Input() singleRp:Recipie
   constructor() { }
 
   ngOnInit(): void {
