16de175a7539ae57b5916a60e93f8e8abb9b912c
lkp25 <lkp25@o2.pl>
2021-08-30 15:18:33 +0200

    added anchortags for main manu functionality - either recipies or shopping list is displayed in the view

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  title = 'myapp';
+  @Output() menuItemClicked = new EventEmitter<{itemName:string}>()
+  
+  onMenuItemClicked(item:string){
+    this.menuItemClicked.emit({itemName:item})
+  }
 }
