2f73ea4aeea2b1f8d21abb9e0fed8867729e660c
lkp25 <lkp25@o2.pl>
2021-09-22 12:35:53 +0200

    applied subject storage in propery and unsubscribing it in onDestroy in shoppingListComponent

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -14,10 +14,10 @@ export class ShoppingListService{
     }
     addIngredient(ingredient: Ingredient){
         this.ingredients.push(ingredient)
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
     }
     addIngredients(ingredients: Ingredient[]){
         this.ingredients.push(...ingredients)
-        this.ingredientsChanged.emit(this.ingredients.slice())
+        this.ingredientsChanged.next(this.ingredients.slice())
     }
 }
