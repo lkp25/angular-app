4a74f1031f24b7750bf786b092bfe01ad1b4813b
lkp25 <lkp25@o2.pl>
2021-10-10 12:54:27 +0200

    CORECTLY SET AUTHORIZATION HEADER to send the JWT - verification on server passes

--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators"
-import { Subject, throwError } from "rxjs"
+import { BehaviorSubject, Subject, throwError } from "rxjs"
 import { User } from "./user.model"
 
 
@@ -10,14 +10,15 @@ import { User } from "./user.model"
 interface AuthResponseData{
     expIn: string,
     message: string,
-    token: string
+    accessToken: string
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService{
 
-
-    user = new Subject<User>()
+    //behavior subject because data service needs access to user value
+    //at any point of time not just when it changes and was emitted on login.
+    user = new BehaviorSubject<User>(null)
 
     constructor(private http: HttpClient) {}
 
@@ -40,8 +41,10 @@ export class AuthService{
             return throwError(error)
             }), 
             tap(data=> { 
+                console.log(data);
+                
                 const expDate = data.expIn
-                const token = data.token
+                const token = data.accessToken
                 const user = new User(userData.name, token, expDate)
                 
                 //emit the constructed user with his token
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth/auth.service';
 import { Recipie } from '../recipies/recipe-model';
 import { RecipieService } from '../recipies/recipie.service';
+import { take } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -10,11 +12,13 @@ export class DataStorageService {
 
   constructor(
     private http: HttpClient,
-    private recipieService: RecipieService
+    private recipieService: RecipieService,
+    private authService: AuthService
     ) { }
 
     recipies = this.recipieService.getRecipies()
-
+    
+    
 
     storeRecipies(){
       console.log(this.recipieService.getRecipies());
@@ -28,8 +32,28 @@ export class DataStorageService {
     }
 
     getAllRecipes(){
-      this.http.get<Recipie[]>('http://localhost:8080/get-all').subscribe(response => {
+      let token: string
+      this.authService.user.pipe(take(1))
+      .subscribe(user =>{
+        console.log(user);
+        
+        token = user.token
+       
+      })
+      
+      
+      
+
+      this.http.get<Recipie[]>('http://localhost:8080/get-all', 
+      {
+        headers: new HttpHeaders().append('Authorization', `Bearer ${token}`)
+      })
+      .subscribe(response => {
         console.log(response);
+
+        //subscribe t the user Subject from authService to get token!
+        //
+        
         this.recipieService.loadRecipiesFromServer(response)
       })
     }
