752b5537eefd53ab0c06c930f9b7c1f575a2c5ee
lkp25 <lkp25@o2.pl>
2021-11-07 20:53:04 +0100

    live crypto price update works

--- src/app/crypto/crypto.component.ts
+++ src/app/crypto/crypto.component.ts
@@ -1,26 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { CryptoService } from './crypto.service';
 
 @Component({
   selector: 'app-crypto',
   templateUrl: './crypto.component.html',
-  styleUrls: ['./crypto.component.css']
+  styleUrls: ['./crypto.component.css'],
 })
-export class CryptoComponent implements OnInit {
-  public cryptoPrices
-
-  constructor(
-    private cryptoService: CryptoService
-  ) { }
+export class CryptoComponent implements OnInit, OnDestroy {
+  public cryptoPrices;
+  private cryptoInterval;
+  constructor(private cryptoService: CryptoService) {}
 
   ngOnInit(): void {
-     this.cryptoService.getAllCrypto().subscribe(data => {
-      console.log(data)
-      this.cryptoPrices = data
-    },
-    error => console.log(error)
-    )
+    //initial load
+    this.getAllCryptoPrices()
+    //interval load
+    this.cryptoInterval = setInterval(() => {
+     this.getAllCryptoPrices()
+    }, 10000);
   }
 
+  getAllCryptoPrices(){
+    this.cryptoService.getAllCrypto().subscribe(
+      (data) => {
+        console.log(data);
+        this.cryptoPrices = data;
+      },
+      (error) => console.log(error)
+    );
+  }
+  ngOnDestroy(){
+    clearInterval(this.cryptoInterval)
+  }
 }
