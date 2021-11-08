0dd0b0eaa1edba4421fb48b20d92ff6860778092
lkp25 <lkp25@o2.pl>
2021-11-08 09:17:22 +0100

    added chart js - first test chart works

--- src/app/crypto/crypto.component.ts
+++ src/app/crypto/crypto.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+import { interval } from 'rxjs';
 
 import { CryptoService } from './crypto.service';
 
@@ -10,9 +12,19 @@ import { CryptoService } from './crypto.service';
 export class CryptoComponent implements OnInit, OnDestroy {
   public cryptoPrices;
   private cryptoInterval;
-  constructor(private cryptoService: CryptoService) {}
+  constructor(
+    private cryptoService: CryptoService,
+    ) {
+      Chart.register(...registerables)
+    }
+  @ViewChild('myChart', {static:true}) chart: ElementRef
+  chartCTX
+  
 
   ngOnInit(): void {
+    
+
+
     //initial load
     this.getAllCryptoPrices()
     //interval load
@@ -33,4 +45,26 @@ export class CryptoComponent implements OnInit, OnDestroy {
   ngOnDestroy(){
     clearInterval(this.cryptoInterval)
   }
+
+ 
+  doChart(){
+    this.chartCTX = this.chart.nativeElement.getContext('2d')
+    // console.log(this.chart.nativeElement);
+    // console.log(this.chartCTX);
+    
+    const net = new Chart(this.chartCTX, {
+      type: 'line', //horizontalBar, doughnut, line pie radar polar area 
+      data: {
+        labels: ['boston', 'worcestrer', 'spring', 'jira', 'angular'],
+        datasets: [{
+          label: 'population',
+          data: [12345, 9234, 45123, 65326, 98441]
+        }]
+      },
+      options: {}
+  }) 
+  }
+  
+
+  
 }
