11c7babf21281139ec175210a8446c151a271ff7
lkp25 <lkp25@o2.pl>
2021-11-17 14:34:56 +0100

    correctly set up file upload functionality with error handling

--- src/app/courses/file-upload/file-upload.component.ts
+++ src/app/courses/file-upload/file-upload.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-file-upload',
@@ -9,7 +12,38 @@ export class FileUploadComponent implements OnInit {
   @Input()
   requiredFileType: string
   fileName = ""
-  constructor() { }
+  fileUploadError = false
+  acceptedFileTypes=['.png','.jpg','.jpeg']
+
+  constructor(private http: HttpClient) { }
+
+  onFileSelected(event){
+    const file:File = event.target.files[0]
+    if(file){
+      this.fileName = file.name
+      console.log(file);
+
+      //append file to a payload:
+      const formData = new FormData()
+      formData.append("thumbnail", file)
+
+      //set the error flag to false for now:
+      this.fileUploadError = false
+
+      this.http.post('http://localhost:8080/files/upload-image', 
+      formData
+      ).pipe(
+        //handle error first if any
+        catchError(error =>{
+          this.fileUploadError = true
+          //emit new observable containing error object and complete the stream
+          return of(error)
+        })
+      )
+      .subscribe()
+    }
+    
+  }
 
   ngOnInit(): void {
   }
